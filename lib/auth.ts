f9c96d3e3eb0dcc91f56e56a7fb554644fe83b90
Fnadresena00 <anjarafandresena05@gmail.com>
2025-12-18 00:32:11 +0200

    override

--- lib/auth.ts
+++ lib/auth.ts
@@ -82,6 +82,7 @@ export const auth = betterAuth({
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      overrideUserInfoOnSignIn: true,
     },
   },
 });
